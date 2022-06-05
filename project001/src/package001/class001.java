0220a73f7d6118c58c93e02e246f5933c21f22d4
ADMIN <ADMIN@192.168.11.6>
2022-06-06 00:27:37 +0900

    20220606 project start

--- project001/src/package001/class001.java
+++ project001/src/package001/class001.java
@@ -3,22 +3,21 @@ package package001;
 import java.util.Scanner;
 
 public class class001 {
-    static Scanner sc = new Scanner(System.in);
 	static String a = "pass";
-    static String b = "fail";
-    static int numA;
-    static int numB;
-    static int result;
-    
+	static String b = "fail";
+	static int numA;
+	static int numB;
+	static int result;
+
 	public static void main(String[] args) {
-	   numA = sc.nextInt();
-	   numB = sc.nextInt();
-	   result = numA+numB;
-	   
+		Scanner sc = new Scanner(System.in);
+		numA = sc.nextInt();
+		numB = sc.nextInt();
+		result = numA + numB;
 		if (result >= 4) {
 			System.out.println(a);
 		} else {
-			System.out.println(b);	   
+			System.out.println(b);
 		}
 	}
 }
