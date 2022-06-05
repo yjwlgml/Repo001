8e510f1c9a25927f9e7c523f2a61a95fbce34656
ADMIN <ADMIN@192.168.11.6>
2022-06-05 23:36:49 +0900

    test

--- HandmadeProjeck/src/package001/class001.java
+++ HandmadeProjeck/src/package001/class001.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class class001 {
     static Scanner sc = new Scanner(System.in);
 	static String a = "pass";
-    static String c = "fail";
+    static String b = "fail";
     static int numA;
     static int numB;
     static int result;
@@ -18,7 +18,7 @@ public class class001 {
 		if (result >= 3) {
 			System.out.println(a);	
 		} else {
-			System.out.println(c);	   
+			System.out.println(b);	   
 		}
 	}
 }
