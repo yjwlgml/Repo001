1522c57d20d7d71fb6e81341d84896352cfd6991
ADMIN <ADMIN@192.168.11.6>
2022-06-05 23:13:06 +0900

    2022/06/05 study start

--- HandmadeProjeck/src/package001/class001.java
+++ HandmadeProjeck/src/package001/class001.java
@@ -1,1 +1,24 @@
 package package001;
+
+import java.util.Scanner;
+
+public class class001 {
+    static Scanner sc = new Scanner(System.in);
+	static String a = "pass";
+    static String b = "fail";
+    static int numA;
+    static int numB;
+    static int result;
+    
+	public static void main(String[] args) {
+	   numA = sc.nextInt();
+	   numB = sc.nextInt();
+	   result = numA+numB;
+	   
+		if (result >= 3) {
+			System.out.println(a);	
+		} else {
+			System.out.println(b);	
+		}
+	}
+}
